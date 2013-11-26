b00b379a1c54d8a65f21b428ca4657221a117d93
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-11-26 17:13:55 +0700

    some packaging stuff

--- flosculus/cli.py
+++ flosculus/cli.py
@@ -8,13 +8,14 @@ Options:
     -v --version    Show version.
 """
 from .core import Flosculus
+from ._meta import __version__
 
 import configparser
 from docopt import docopt
 
 
 def main():
-    arguments = docopt(__doc__, version="v0.1")
+    arguments = docopt(__doc__, version="v{}".format(__version__))
 
     config = configparser.ConfigParser()
     config.read(arguments["<config>"])
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from flosculus import _meta
+
+
+setup(
+    name="flosculus",
+    version=_meta.__version__,
+    description="",
+    long_description="",
+    author="Isman Firmansyah",
+    author_email="isman.firmansyah@gmail.com",
+    url="https://github.com/iromli/flosculus",
+    packages=["flosculus"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+    entry_points={
+        "console_scripts": ["flosculusd=flosculus.cli:main"],
+    },
+    zip_safe=False,
+)
