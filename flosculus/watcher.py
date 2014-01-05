393d714ca1d89e59831588d079e6faa3af5ba4b6
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-01-05 17:30:09 +0700

    cleanup watcher.py

--- flosculus/watcher.py
+++ flosculus/watcher.py
@@ -20,16 +20,6 @@ class LogWatcher(object):
     """Looks for changes in all files of a directory.
     This is useful for watching log file changes in real-time.
     It also supports files rotation.
-
-    Example:
-
-    .. sourcecode:: python
-
-        def callback(filename, lines):
-            print(filename, lines)
-
-        lw = LogWatcher("/var/log/", callback)
-        lw.loop()
     """
 
     def __init__(self, files, callback, tail_lines=0, sizehint=1048576):
@@ -52,17 +42,16 @@ class LogWatcher(object):
         self._sizehint = sizehint
 
         self.update_files()
-        for id, file in self._files_map.items():
-            file.seek(os.path.getsize(file.name))  # EOF
+        for _, file_ in self._files_map.iteritems():
+            file_.seek(os.path.getsize(file_.name))  # EOF
             if tail_lines:
                 try:
-                    lines = self.tail(file.name, tail_lines)
+                    lines = self.tail(file_.name, tail_lines)
+                    if lines:
+                        self._callback(file_.name, lines)
                 except IOError as err:
                     if err.errno != errno.ENOENT:
                         raise
-                else:
-                    if lines:
-                        self._callback(file.name, lines)
 
     def __enter__(self):
         return self
@@ -83,8 +72,8 @@ class LogWatcher(object):
         # than first checking file's last modification times.
         while True:
             self.update_files()
-            for fid, file in list(self._files_map.items()):
-                self.readlines(file)
+            for _, file_ in self._files_map.iteritems():
+                self.readlines(file_)
             if not blocking:
                 return
             time.sleep(interval)
@@ -94,7 +83,7 @@ class LogWatcher(object):
         logger.info(line)
 
     @classmethod
-    def open(cls, file):
+    def open(cls, file_):
         """Wrapper around open().
         By default files are opened in binary mode and readlines()
         will return bytes on both Python 2 and 3.
@@ -114,7 +103,7 @@ class LogWatcher(object):
                 errors='ignore',
             )
         """
-        return open(file, 'rb')
+        return open(file_, 'rb')
 
     @classmethod
     def tail(cls, fname, window):
@@ -160,76 +149,72 @@ class LogWatcher(object):
         for name in self._files:
             try:
                 st = os.stat(name)
-            except EnvironmentError as err:
-                if err.errno != errno.ENOENT:
-                    raise
-            else:
                 if not stat.S_ISREG(st.st_mode):
                     continue
                 fid = self.get_file_id(st)
                 ls.append((fid, name))
-
-        # check existent files
-        for fid, file in list(self._files_map.items()):
-            try:
-                st = os.stat(file.name)
             except EnvironmentError as err:
                 if err.errno != errno.ENOENT:
                     raise
-                self.unwatch(file, fid)
-            else:
+
+        # check existent files
+        for fid, file_ in self._files_map.iteritems():
+            try:
+                st = os.stat(file_.name)
                 if fid != self.get_file_id(st):
                     # same name but different file (rotation);
                     # reload it.
-                    self.unwatch(file, fid)
-                    self.watch(file.name)
+                    self.unwatch(file_, fid)
+                    self.watch(file_.name)
+            except EnvironmentError as err:
+                if err.errno != errno.ENOENT:
+                    raise
+                self.unwatch(file_, fid)
 
         # add new ones
         for fid, fname in ls:
             if fid not in self._files_map:
                 self.watch(fname)
 
-    def readlines(self, file):
+    def readlines(self, file_):
         """Read file lines since last access until EOF is reached and
         invoke callback.
         """
         while True:
-            lines = file.readlines(self._sizehint)
+            lines = file_.readlines(self._sizehint)
             if not lines:
                 break
-            self._callback(file.name, lines)
+            self._callback(file_.name, lines)
 
     def watch(self, fname):
         try:
-            file = self.open(fname)
+            file_ = self.open(fname)
             fid = self.get_file_id(os.stat(fname))
+            self.log("watching logfile {}".format(fname))
+            self._files_map[fid] = file_
         except EnvironmentError as err:
             if err.errno != errno.ENOENT:
                 raise
-        else:
-            self.log("watching logfile {}".format(fname))
-            self._files_map[fid] = file
 
-    def unwatch(self, file, fid):
+    def unwatch(self, file_, fid):
         # File no longer exists. If it has been renamed try to read it
         # for the last time in case we're dealing with a rotating log
         # file.
-        self.log("un-watching logfile {}".format(file.name))
+        self.log("un-watching logfile {}".format(file_.name))
         del self._files_map[fid]
 
-        with file:
-            lines = self.readlines(file)
+        with file_:
+            lines = self.readlines(file_)
             if lines:
-                self._callback(file.name, lines)
+                self._callback(file_.name, lines)
 
     @staticmethod
     def get_file_id(st):
         if os.name == 'posix':
             return "{:x}g{:x}".format(st.st_dev, st.st_ino)
-        else:
-            return "{:f}".format(st.st_ctime)
+        return "{:f}".format(st.st_ctime)
 
     def close(self):
-        for id, file in self._files_map.items():
-            file.close()
+        for _, file_ in self._files_map.iteritems():
+            file_.close()
         self._files_map.clear()
